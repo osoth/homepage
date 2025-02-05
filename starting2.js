396b0bfcf7c1c97774530baef88b0aca957feb29
Oskar Soth <oskar.soth@uni-potsdam.de>
2025-02-05 11:21:21 +0100

    update project

--- starting2.js
+++
@@ -1,9 +0,0 @@
-function switchStylesheet() {
-    var currentStyleSheet = document.getElementById("backgrounddefault");
-    currentStyleSheet.disabled = true;
-    var newStyleSheet = document.createElement("link");
-    newStyleSheet.rel = "stylesheet";
-    newStyleSheet.href = "loginpage.css";
-    newStyleSheet.id = "loginStylesheet";
-    document.head.appendChild(newStyleSheet);
-}
